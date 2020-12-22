a384427f17edc7295c04c90c9a18b9f63c894e26
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-22 19:31:19 +0100

    Basic props like level, pointsToPassLevel and playerMovement  of gameLevels file, where are all levels for this game

---
+++ javascript/gameData/gameLevels.esm.js
@@ -0,0 +1,15 @@
+export const gameLevels = [
+    {
+        level: 1,
+        pointsToPassLevel: 5000,
+        playerMovement: 30,
+    },{
+        level: 2,
+        pointsToPassLevel: 8000,
+        playerMovement: 35,
+    },{
+        level: 3,
+        pointsToPassLevel: 10000,
+        playerMovement: 40,
+    }
+],
