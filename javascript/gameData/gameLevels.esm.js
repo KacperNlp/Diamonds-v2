68fc409505862a2aec41dbb1702bb10b2eb9476c
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-22 20:00:24 +0100

    basic styles and javascript service of levels layer

--- javascript/gameData/gameLevels.esm.js
+++ javascript/gameData/gameLevels.esm.js
@@ -1,15 +1,18 @@
 export const gameLevels = [
     {
         level: 1,
+        unlocked: true,
         pointsToPassLevel: 5000,
         playerMovement: 30,
     },{
         level: 2,
+        unlocked: false,
         pointsToPassLevel: 8000,
         playerMovement: 35,
     },{
         level: 3,
+        unlocked: false,
         pointsToPassLevel: 10000,
         playerMovement: 40,
     }
-],
+];
--- javascript/scripts/LevelsLayer.esm.js
+++ javascript/scripts/LevelsLayer.esm.js
@@ -1,41 +1,46 @@
 import {WorkiWithHtml, markups} from './WorkiWithHtml.esm.js';
+import {gameLevels} from '../gameData/gameLevels.esm.js'
 
 const UNLOCKED_LEVEL_BUTTON_CLASS = 'unlocked';
 const LOCKED_LEVEL_BUTTON_CLASS = 'locked';
 const LEVELS_LAYER_ID = 'js-levels';
+const LEVEL_BUTTONS_CONTAINER_ID = 'js-levels-container';
+const LEVEL_BUTTONS_CLASS = 'level-button';
 
-const gameLevels = [
-    {
-        level: 1,
-        unlocked: true,
-    },{
-        level: 2,
-        unlocked: false,
-    }
-]
 //this class shows levels layer with all level (locked and unlocked)
 class LevelsLayer extends WorkiWithHtml{
     constructor(){
         super(LEVELS_LAYER_ID)
+        this.buttonsContainer = this.bindToElement(LEVEL_BUTTONS_CONTAINER_ID);
+        this.addLevelButtonsToLevelsLayer();
     }
 
     addLevelButtonsToLevelsLayer(){
+        this.#clearLevelButtonsContainer();
+        this.#createLevelButtons();
+    }
 
-        for(let counter = 0; counter <= gameLevels; counter++){
+    #clearLevelButtonsContainer(){
+        while(this.buttonsContainer.firstChild){
+            this.buttonsContainer.removeChild(this.buttonsContainer.firstChild)
+        }
+    }
+
+    #createLevelButtons(){
+        for(let counter = 0; counter < gameLevels.length; counter++){
 
             const button = this.createElement(markups.button);
             const {level, unlocked} = gameLevels[counter]
 
             this.#typeOfLevelButton(button, unlocked, level);
-            this.element.appendChild(button);
 
+            this.buttonsContainer.appendChild(button)
         }
-
     }
 
     #typeOfLevelButton(button, isUnlocked, txt){
         button.textContent = txt;
-        button.classList.add('button');
+        button.classList.add(LEVEL_BUTTONS_CLASS);
         if(isUnlocked) button.classList.add(UNLOCKED_LEVEL_BUTTON_CLASS)
         else  button.classList.add(LOCKED_LEVEL_BUTTON_CLASS)
     }
