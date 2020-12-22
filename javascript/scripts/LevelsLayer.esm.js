418e5b0eb5058c495a4441bf39f532ff55e86217
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-22 20:51:38 +0100

    improved LevelsLayer by settings on level buttons new attributes, thanks to them will be able chekcs the button level and status

--- javascript/scripts/LevelsLayer.esm.js
+++ javascript/scripts/LevelsLayer.esm.js
@@ -8,9 +8,11 @@ const LOCKED_LEVEL_BUTTON_CLASS = 'locked';
 const LEVELS_LAYER_ID = 'js-levels';
 const LEVEL_BUTTONS_CONTAINER_ID = 'js-levels-container';
 const LEVEL_BUTTONS_CLASS = 'level-button';
+const LEVEL_BUTTONS_NAME_OF_ATTRIBUTE_WITH_LEVEL = 'data-level';
+const LEVEL_BUTTONS_NAME_OF_ATTRIBUTE_WITH_STATUS = 'data-unlocked';
 const RETURN_TO_MENU_BUTTON_ID = 'js-return-to-menu-button';
 
-//this class shows levels layer with all level (locked and unlocked)
+//this class shows levels layer with all levels (locked and unlocked)
 class LevelsLayer extends WorkiWithHtml{
     constructor(){
         super(LEVELS_LAYER_ID)
@@ -53,9 +55,14 @@ class LevelsLayer extends WorkiWithHtml{
         }
     }
 
-    #typeOfLevelButton(button, isUnlocked, txt){
-        button.textContent = txt;
+    //if level is unlocked than he will has a different styles and he will receive an event listener
+    #typeOfLevelButton(button, isUnlocked, level){
+
+        button.textContent = level;
+        button.setAttribute(LEVEL_BUTTONS_NAME_OF_ATTRIBUTE_WITH_LEVEL, level);
+        button.setAttribute(LEVEL_BUTTONS_NAME_OF_ATTRIBUTE_WITH_STATUS, isUnlocked);
         button.classList.add(LEVEL_BUTTONS_CLASS);
+
         if(isUnlocked) button.classList.add(UNLOCKED_LEVEL_BUTTON_CLASS)
         else  button.classList.add(LOCKED_LEVEL_BUTTON_CLASS)
     }
