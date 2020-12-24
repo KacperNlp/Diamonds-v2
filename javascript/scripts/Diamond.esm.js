ced622f96cb5802bfd58cc3358810b70a89a4dee
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-24 21:19:57 +0100

    DiamondSprite created to draw diamond, by his position and kind

--- javascript/scripts/Diamond.esm.js
+++ javascript/scripts/Diamond.esm.js
@@ -1,10 +1,11 @@
 import {DiamondSprite} from './DiamondSprite.esm.js'
 
-const DIAMOND_WIDTH = 46;
-const DIAMOND_HEIGHT = 48;
+export const DIAMOND_WIDTH = 46;
+export const DIAMOND_HEIGHT = 48;
 export class Diamond extends DiamondSprite{
     constructor(posX, posY, row, column, kind, imageSprite){
-        super(posX, posY, row, column, imageSprite);
+        this.alpha = 255;
+        super(posX, posY, row, column, imageSprite, this.alpha);
         this.kind = kind;
     }
 }
--- javascript/scripts/DiamondSprite.esm.js
+++ javascript/scripts/DiamondSprite.esm.js
@@ -1,7 +1,36 @@
 import {canvas} from './Canvas.esm.js';
+import {DIAMOND_WIDTH, DIAMOND_HEIGHT} from './Diamond.esm.js'
 
-export class DiamondSprite extends canvas{
-    constructor(){
+export class DiamondSprite{
+    constructor(posX, posY, row, column, numberOfKind, imageSprite, alpha){
+
         super();
+
+        this.posX = posX;
+        this.posY = posY;
+        this.row = row;
+        this.column = column;
+        this.numberOfKind = numberOfKind;
+        this.imageSprite = imageSprite;
+        this.alpha = alpha;
+
+    }
+
+    drawDiamond(){
+
+        if(this.alpha !== 255) canvas.ctx.globalAlpha = this.alpha/255;
+
+        canvas.ctx.drawImage(
+            this.imageSprite, 
+            this.numberOfKind * DIAMOND_WIDTH,
+            0,
+            DIAMOND_WIDTH,
+            DIAMOND_HEIGHT,
+            this.posX,
+            this.posY,
+            DIAMOND_WIDTH,
+            DIAMOND_HEIGHT,
+        )
+
     }
 }
