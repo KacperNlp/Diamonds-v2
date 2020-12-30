a6b23e2b3805e0509451792c1fdf2e40a2c2af43
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-30 23:18:27 +0100

    MovementPossibilities has been creating to check possibility movement

---
+++ javascript/scripts/MovementPossibilities.esm.js
@@ -0,0 +1,35 @@
+import { EMPTY_BLOCK, NUMBER_OF_COLUMNS, NUMBER_OF_ROWS } from '../gameData/gameLevels.esm.js';
+import {game} from './Game.esm.js'
+
+class MovementPossibilities{
+    checkPossibilityMovement(){
+        if(game.gameState.getIsMoving()) return;
+
+        game.gameState.getGameMap().some((diamond, index, diamonds) =>{
+
+            if(diamond.kind === EMPTY_BLOCK) return false;
+
+            //move right in row
+            if(
+                index % NUMBER_OF_COLUMNS < NUMBER_OF_COLUMNS - 3
+                && diamond.kind === diamonds[index + 2].kind
+                && diamond.kind === diamonds[index + 3].kind
+            ){
+                return true;
+            }
+
+            //move right (diamond is in middle of the column)
+            if(
+                index % NUMBER_OF_COLUMNS < NUMBER_OF_COLUMNS - 1
+                && Math.floor(index / NUMBER_OF_COLUMNS) > 1
+                && Math.floor(index /NUMBER_OF_COLUMNS) < NUMBER_OF_ROWS - 1
+                && diamond.kind === diamonds[index + NUMBER_OF_COLUMNS].kind
+                && diamond.kind === diamonds[index - NUMBER_OF_COLUMNS].kind
+            ){
+                return true;
+            }
+        })
+    }
+}
+
+export const movementPossibilities = new MovementPossibilities();
