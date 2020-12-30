ee9fc7c1f03c73fa3d8b930b771a028c8bf132a7
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-30 21:33:35 +0100

    Created UserData where you can checks whether the level is unlocked, get hight score and set new hisght score

---
+++ javascript/scripts/UserData.esm.js
@@ -0,0 +1,38 @@
+class UserData{
+    constructor(){
+        if(!localStorage.length){
+            localStorage.setItem('1', JSON.stringify({active:true, score:0,}));
+        }
+    }
+
+    checkAvailabilityLevel(level){
+        const lvl = localStorage.getItem(String(level));
+
+        if(!lvl) return;
+
+        const {active} = JSON.parse(lvl);
+
+        return active;
+    }
+
+    unlockNewLevel(level){
+        localStorage.setItem(String(level), JSON.stringify({active:true, score:0}));
+    }
+
+    getHighestScore(level){
+        const lvl = localStorage.getItem(String(level));
+
+        if(!level) return;
+
+        const {score} = JSON.parse(lvl);
+
+        return score;
+    }
+
+    setNewHightScore(level, newScore){
+        localStorage.setItem(String(level), JSON.stringify({active:true, score: newScore}))
+    }
+
+}
+
+export const userData = new UserData();
