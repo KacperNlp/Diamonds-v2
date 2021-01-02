8a5676590a35e21ba939062f845e02ada2cd33e9
Kacper Nalepa <kacper.nlp@wp.pl>
2021-01-02 21:10:20 +0100

    Created functions in Media to handle diamonds swap sound and background music

--- javascript/scripts/Media.esm.js
+++ javascript/scripts/Media.esm.js
@@ -2,9 +2,85 @@ class Media{
 
     constructor(){
         this.isInLevel = false;
+        this.musicVolume = .3;
+        this.swapVolume = .3;
+        this.allowedMusic = true;
+        this.allowedSwapSound = true;
     }
 
     #diamondSprite = null;
+    #swapSound = null;
+    #backgroundMusic = null;
+
+    //music
+    increaseMusicVolume(){
+        this.musicVolume += .1;
+
+        if(this.musicVolume > 1) this.musicVolume = 1;
+
+        if(this.#backgroundMusic) this.#backgroundMusic.volume = this.musicVolume;
+    }
+
+    decreaseMusicVolume(){
+        this.musicVolume -= .1;
+
+        if(this.musicVolume < 0) this.musicVolume = 0;
+
+        if(this.#backgroundMusic) this.#backgroundMusic.volume = this.musicVolume;
+    }
+
+    playBackgroundMusic(){
+        if(!this.allowedMusic) return;
+
+        this.#backgroundMusic.loop = true;
+        if(this.#backgroundMusic) this.#backgroundMusic.play();
+    }
+
+    stopBackgroundMusic(){
+        if(this.#backgroundMusic) this.#backgroundMusic.pause();
+    }
+
+    //swap sound
+    increaseSwapSound(){
+        this.swapVolume += .1;
+
+        if(this.swapVolume > 1) this.swapVolume = 1;
+
+        if(this.#swapSound)  this.#swapSound.volume = this.swapVolume;
+    }
+
+    decreaseSwapSound(){
+        this.swapVolume -= .1;
+
+        if(this.swapVolume < 0) this.swapVolume = 0;
+
+        if(this.#swapSound) this.#swapSound.volume = this.swapVolume;
+    }
+
+    playSwapSound(){
+        if(!this.allowedSwapSound) return;
+
+        if(this.#swapSound) this.#swapSound.play();
+    }
+
+    //get and set swap sound and background music
+    set swapSound(sound){
+        this.#swapSound = sound;
+        this.#swapSound.volume = this.swapVolume;
+    }
+
+    get swapSound(){
+        return !!this.#swapSound;
+    }
+
+    set backgroundMusic(music){
+        this.#backgroundMusic = music;
+        this.#backgroundMusic.volume = this.musicVolume;
+    }
+
+    get backgroundMusic(){
+        return !!this.#backgroundMusic;
+    }
 
     get diamondSprite(){
         return this.#diamondSprite;
